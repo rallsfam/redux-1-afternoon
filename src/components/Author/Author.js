f1b62eaacfff85b004f62c4e10043ede326881bc
Jason Ralls <admin@MacBook-Pro.routerlogin.net>
2020-07-07 17:17:03 -0500

    my changes

--- src/components/Author/Author.js
+++ src/components/Author/Author.js
@@ -1,59 +1,27 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import store, { UPDATE_AUTHOR_FIRST, UPDATE_AUTHOR_LAST } from "./../../store";
 import './Author.css';
 
 class Author extends Component {
   constructor(props) {
     super(props);
+    const reduxState = store.getState();
     this.state = {
-      authorFirst: '',
-      authorLast: ''
+      authorFirst: reduxState.authorFirst,
+      authorLast: reduxState.authorLast
     };
   }
-
-  handleAuthorFirstChange(nameVal) {
-    this.setState({
-      authorFirst: nameVal
-    });
-  }
-
-  handleAuthorLastChange(nameVal) {
-    this.setState({
-      authorLast: nameVal
-    });
-  }
+  
   saveChanges() {
-    // Send data to Redux state
-  }
-  render() {
-    return (
-      <div className="Author forms">
-        <div className="input_container">
-          <h2>Author First Name:</h2>
-          <input
-            value={this.state.authorFirst}
-            onChange={e => this.handleAuthorFirstChange(e.target.value)}
-          />
-        </div>
-        <div className="input_container">
-          <h2>Author Last Name:</h2>
-          <input
-            value={this.state.authorLast}
-            onChange={e => this.handleAuthorLastChange(e.target.value)}
-          />
-        </div>
-        <Link to="/add/name">
-          <button onClick={() => this.saveChanges()} className="left_button">
-            Previous
-          </button>
-        </Link>
-        <Link to="/add/ingredients">
-          <button onClick={() => this.saveChanges()} className="right_button">
-            Next
-          </button>
-        </Link>
-      </div>
-    );
+    store.dispatch({
+      type: UPDATE_AUTHOR_FIRST,
+      payload: this.state.authorFirst
+    });
+    store.dispatch({
+      type: UPDATE_AUTHOR_LAST,
+      patload: this.state.authorLast
+    });
   }
 }
 
--- src/components/Name/Name.js
+++ src/components/Name/Name.js
@@ -1,62 +1,15 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import "./Name.css";
+// import React, { Component } from "react";
+// import {Link } from "react-router-dom";
+// import store, { UPDATE_NAME, UPDATE_CATEGORY } from "./../../store";
 
 class Name extends Component {
   constructor(props) {
     super(props);
+    const reduxState = store.getState();
     this.state = {
-      name: '',
-      category: ''
+      name: reduxState.name,
+      category: reduxState.category
     };
-  }
-  handleNameChange(nameVal) {
-    this.setState({
-      name: nameVal
-    });
-  }
-
-  handleCategoryChange(catVal) {
-    this.setState({
-      category: catVal
-    });
-  }
-  saveChanges() {
-    // Send data to Redux state
-  }
-  render() {
-    return (
-      <div className="Name forms">
-        <div className="input_container">
-          <h2>Recipe Name:</h2>
-          <input
-            value={this.state.name}
-            onChange={e => this.handleNameChange(e.target.value)}
-          />
-        </div>
-        <div className="input_container">
-          <h2>Category:</h2>
-          <select
-            value={this.state.category}
-            onChange={e => this.handleCategoryChange(e.target.value)}
-          >
-            <option value={""}>----</option>
-            <option value={"Breakfast"}>Breakfast</option>
-            <option value={"Second Breakfast"}>Second Breakfast</option>
-            <option value={"Brunch"}>Brunch</option>
-            <option value={"Lunch"}>Lunch</option>
-            <option value={"Dinner"}>Dinner</option>
-            <option value={"Drinks"}>Drinks</option>
-            <option value={"Dessert"}>Dessert</option>
-          </select>
-        </div>
-        <Link to="/add/author">
-          <button onClick={() => this.saveChanges()} className="right_button">
-            Next
-          </button>
-        </Link>
-      </div>
-    );
   }
 }
 
